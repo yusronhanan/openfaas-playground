7a3ca8b90b037b3640772b731567b32b587e81dd
Yusron H <yusronzain@gmail.com>
2022-11-30 11:43:36 +0700

    add ping2

--- ping/ping/handler.py
+++ ping/ping/handler.py
@@ -1,7 +1,14 @@
+import random
+import time
 def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
-
-    return req
+    quotes = [
+        'Neo: We need guns. Lots of guns.',
+        'Switch: Great, the digital pimp at work.',
+        'Trinity: Dodge this.',
+        "Neo: I don't like the idea that I'm not in control of my life.",
+        'Neo: There is no spoon!',
+        'Cypher: Ignorance is bliss.'
+    ]
+    time.sleep(random.randint(10,30))
+    
+    return random.choice(quotes)
